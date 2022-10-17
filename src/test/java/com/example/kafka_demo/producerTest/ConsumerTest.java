64c2e8a1fc169559fad31c3bbb6b3e3b7276becd
ConnorSu <bunny0083@gmail.com>
2022-10-17 18:28:17 +0800

    add assignment strategy

--- src/test/java/com/example/kafka_demo/producerTest/ConsumerTest.java
+++ src/test/java/com/example/kafka_demo/producerTest/ConsumerTest.java
@@ -6,6 +6,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,18 +24,22 @@ import java.util.Properties;
  */
 @SpringBootTest
 public class ConsumerTest {
-
-   @Test
-   void consumer1(){
-      Properties properties = new Properties();
-
+  private static Properties properties = new Properties();
+   @BeforeAll
+   public static void init(){
       properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "K3:9092,K5:9092");
       properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
       properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
+      properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.RoundRobinAssignor");
       //設定groupid
       properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
 
+   }
+
+
+   @Test
+   void consumer1(){
       KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
 
       List<String> topics = List.of("first");
@@ -49,15 +55,6 @@ public class ConsumerTest {
 
    @Test
    void consumer2(){
-      Properties properties = new Properties();
-
-      properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "K3:9092,K5:9092");
-      properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-      properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
-      //設定groupid
-      properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-
       KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
 
       List<String> topics = List.of("first");
@@ -73,15 +70,6 @@ public class ConsumerTest {
 
    @Test
    void consumer3(){
-      Properties properties = new Properties();
-
-      properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "K3:9092,K5:9092");
-      properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-      properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
-      //設定groupid
-      properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-
       KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
 
       List<String> topics = List.of("first");
